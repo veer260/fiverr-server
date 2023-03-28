e554d77b3dba55b82386d6aca9515ab0772c0137
veer260 <57021124+veer260@users.noreply.github.com>
2023-03-28 06:26:17 +0530

    added frontend url

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const connectDB = async () => {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://fiverr-app-hazel.vercel.app/"],
     credentials: true,
   })
 );
